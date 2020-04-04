6f9beea585c5c4debcf3ae4f0b2bdc433df4f53b
Csepregi <csepregigabor02@gmail.com>
2020-04-04 16:28:27 +0200

    use axios instead of fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useReducer, useCallback } from 'react';
+import axios from 'axios'
 import './App.css';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
@@ -126,13 +127,13 @@ const App = () => {
   const handleFetchStories = useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
-    fetch(`${API_ENDPOINT}${searchTerm}`)
-      .then(response => response.json())
+    axios
+      .get(url)
       .then(result => {
         console.log(result)
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits
+          payload: result.data.hits
         })
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }))
